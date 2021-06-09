a083f0271ebf76d614b2e81d5abc42ad33b171d9
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-09 09:25:03 -0400

    first commit

---
+++ src/ReadPDF.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author SOLON
+ *
+ */
+public class ReadPDF {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
