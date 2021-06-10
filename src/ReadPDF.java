1d1351c88ddd2effafd14d45f69c7d50a044fe79
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-10 14:55:32 -0400

    adding contents to readpdf and the output text file

--- src/ReadPDF.java
+++ src/ReadPDF.java
@@ -1,9 +1,19 @@
-/**
- * 
- */
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+
+import com.snowtide.PDF;
+import com.snowtide.pdf.Document;
+import com.snowtide.pdf.OutputTarget;
+import com.snowtide.pdf.Page;
 /**
  * @author SOLON
+ * 1. add pdfxstream.jar 
+ * 2. add the jar file to the classpath/build path
+ * 3. 
  *
  */
 public class ReadPDF {
@@ -12,7 +22,23 @@ public class ReadPDF {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String pdf_filename="hello.pdf";
+		File pdfFile= new File(pdf_filename);  // The PDF file from where you would like to extract
+		Document pdf = PDF.open(pdfFile);
+		File txtFile = new File("hello.txt");
+	    Page page = pdf.getPage(0);
+		try {
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(txtFile)));
+		    page.pipe(new OutputTarget(writer));
+		    pdf.close();
+		    writer.flush();
+		    writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	    
+
 
 	}
 
